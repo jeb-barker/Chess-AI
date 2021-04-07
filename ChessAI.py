db831e4f8d26cdf892417e5d123d6999cc64bf7b
jeebtheweeb <48389379+jeebtheweeb@users.noreply.github.com>
2021-04-07 08:41:48 -0400

    error handling

--- ChessAI.py
+++ ChessAI.py
@@ -139,24 +139,34 @@ class Best_AI_bot:
         self.directions = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
         self.opposite_color = {chess.BLACK: chess.WHITE, chess.WHITE: chess.BLACK}
 
-    def best_strategy(self, board, color, depth):
+    def best_strategy(self, board, color, depth, maxTime, startTime):
         # returns best move
-
-        best_move = self.alphabeta(board, color, depth, -10000, 10000)
+        # boardC = board.copy()
+        # best_move = self.alphabeta(board.copy(), color, 1, -10000, 10000, maxTime, startTime)
+        for x in range(1, 100):
+            try:
+                best_move = self.alphabeta(board.copy(), color, x, -10000, 10000, maxTime, startTime)
+                # best_move = best_move2
+                # print(best_move)
+            except TimeoutError:
+                print(x-1)
+                break
         return best_move
 
-    def max_value(self, board, color, search_depth, alpha, beta):
+    def max_value(self, board, color, search_depth, alpha, beta, maxTime, startTime):
         # return value and state: (val, state)
         poss = board.legal_moves
         # print(poss)
         if search_depth == 0 or poss is None or self.is_done(board, color) is True:
             return self.evaluate(board, color, poss)
+        if time.time() - startTime > maxTime:
+            raise TimeoutError
         v = (-1000000, board)
 
         for s in poss:
             board.push(s)
             min = self.min_value(board, chess.WHITE if color == chess.BLACK else chess.BLACK, search_depth - 1, alpha,
-                                 beta)
+                                 beta, maxTime, startTime)
             try:
                 min = min[0]
             except TypeError:
@@ -168,17 +178,19 @@ class Best_AI_bot:
             alpha = max(v[0], alpha)
         return v
 
-    def min_value(self, board, color, search_depth, alpha, beta):
+    def min_value(self, board, color, search_depth, alpha, beta, maxTime, startTime):
         # return value and state: (val, state)
         poss = board.legal_moves
         if search_depth == 0 or poss is None or self.is_done(board, color) is True:
             return -self.evaluate(board, color, poss)
+        if time.time() - startTime > maxTime:
+            raise TimeoutError
         v = (1000000, board)
 
         for s in poss:
             board.push(s)
             maxV = self.max_value(board, chess.WHITE if color == chess.BLACK else chess.BLACK, search_depth - 1, alpha,
-                                  beta)
+                                  beta, maxTime, startTime)
             try:
                 maxV = maxV[0]
             except TypeError:
@@ -191,9 +203,9 @@ class Best_AI_bot:
 
         return v
 
-    def alphabeta(self, board, color, search_depth, alpha, beta):
+    def alphabeta(self, board, color, search_depth, alpha, beta, maxTime, startTime):
         # returns best "value" while also pruning
-        return self.max_value(board, color, search_depth, alpha, beta)
+        return self.max_value(board, color, search_depth, alpha, beta, maxTime, startTime)  # if color else self.min_value(board, color, search_depth, alpha, beta, maxTime, startTime)
 
     def make_move(self, board, color, move):
         # returns board that has been updated
@@ -280,12 +292,13 @@ frame = Canvas(tk, width=size[0], height=size[1])
 frame.pack()
 frame.create_image(0, 0, anchor='nw', image=pimg)
 tk.update()
+print(Best_AI_bot.evaluate(bbot, board, True, []))
 import time
 while True:
     curtime = time.time()
     while True:
         try:
-            move = Best_AI_bot.best_strategy(bbot, board, True, 4)
+            move = Best_AI_bot.best_strategy(bbot, board, True, 4, 10, curtime)
             # move = Candidate_AI_bot.best_strategy(bbot, board, True, 4)
             break
         except ValueError:
@@ -306,7 +319,7 @@ while True:
     if board.is_checkmate():
         print("White wins")
         break
-    if board.is_game_over():
+    if board.is_game_over() or board.can_claim_draw():
         print("Game over")
         break
     lm = [a for a in board.legal_moves]
@@ -319,17 +332,20 @@ while True:
     # board.push(move[1])
     # print(move, " Black: ",time.time() - curtime, "\n")
 
-    # board.push(random.choice(lm))
+    # board.push(random.choice(lm))  # TODO RANDOM
     while True:
         try:
-            move = input()
+            move = input()  # TODO HUMAN INPUT BLACK
+            # move = Best_AI_bot.best_strategy(bbot, board, False, 4, 10, curtime)
             if chess.Move.from_uci(move) not in lm:
                 raise ValueError
             board.push(chess.Move.from_uci(move))
+            # board.push(move[1])
             break
         except ValueError:
             print("ERROR: Illegal Move, Try Again")
     print("black moved")
+    print(move, " Black: ", time.time() - curtime, "\n")
     open('out.svg', 'w').write(chess.svg.board(board, size=500, flipped=True))
     # drawing = svg2rlg("out.svg")
     # renderPM.drawToFile(drawing, "temp.png", fmt="PNG")
@@ -342,7 +358,7 @@ while True:
     if board.is_checkmate():
         print("Black wins")
         break
-    if board.is_game_over():
+    if board.is_game_over() or board.can_claim_draw():
         print("Game over")
         break
     # break
